02b3e8c324198f7a71d075332f4a9527753aff45
mahi-laddha <143066053+mahi-laddha@users.noreply.github.com>
2025-02-03 23:10:16 +0530

    Finding the smallest distance

--- Main.java
+++ Main.java
@@ -8,3 +8,6 @@ public class Main {
         
         // Task 1: Separate even and odd numbers
         ArrayLists.separateEvenOdd(numbers);
+
+        // Task 2: Find smallest distance between neighboring numbers
+        System.out.println("Index of first number with smallest distance: " + ArrayLists.findSmallestDistance(numbers));
